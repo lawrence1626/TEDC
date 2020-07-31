6846f9243a6aba45c7a495f4f2162dc6118e653a
lawrence1626 <lawrencetung26@gmail.com>
2020-07-31 08:22:29 +0800

    Update OECD_data_download_test.py

--- OECD_data_download_test.py
+++ OECD_data_download_test.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import math, re, sys, calendar, os, copy, time
+import pandas as pd
+import numpy as np
+from datetime import datetime, date
 # In[80]:
 
-
+import pandasdmx
 from pandasdmx import Request
-oecd = Request('OECD')
-data_response = oecd.data(resource_id='MEI_BTS_COS', key='all?startTime=1995')
-df = data_response.to_pandas()
-#df.to_csv('c:\\Temp\\test_lei.txt', sep='\t')
 
+tStart = time.time()
+
+oecd = Request('OECD')
+print('Time: ', int(time.time() - tStart),'s'+'\n')
+
+data_response = oecd.data(resource_id='MEI_BTS_COS', key='all?startTime=1995')
+print('Time: ', int(time.time() - tStart),'s'+'\n')
+
+df = data_response.to_pandas()
+print('Time: ', int(time.time() - tStart),'s'+'\n')
+
+#df.to_csv('c:\\Temp\\test_lei.txt', sep='\t')
+path = 'C:/Users/lawre/Desktop'
 
 # In[82]:
 
 
 #df = data_response.to_pandas()
 #type(data_response.data)
-df.to_csv('test_lei.csv')
-
+df.to_csv(path+'test_lei.csv')
+print('Time: ', int(time.time() - tStart),'s'+'\n')
 
 # In[93]:
 
